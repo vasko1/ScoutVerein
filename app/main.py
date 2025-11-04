7cf9b5796578db5ef4588500b38abbfa8dd1fc0b
vasko1 <31367155+vasko1@users.noreply.github.com>
2025-11-04 11:02:31 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -44,6 +44,5 @@ def health():
 
 
 if __name__ == "__main__":
-   
-uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+   uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
