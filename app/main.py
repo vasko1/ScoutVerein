e1e98015ed0dee7bee712a8d2196f31b628ed3a5
vasko1 <31367155+vasko1@users.noreply.github.com>
2025-11-04 09:01:52 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -40,3 +40,10 @@ app.include_router(users_router.router)
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
